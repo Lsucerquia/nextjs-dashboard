b301907cd68e23187eb62fa6e742cf9ad66b8ab7
Lorena Sucerquia <cristi.mayo@hotmail.com>
2024-08-14 14:28:14 -0500

    dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';//cambio la fecha por una linea azul gruesa
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
-  children,
+  children,//llegan todos los componenet sque se dibujen
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+   
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      
     </html>
   );
 }
