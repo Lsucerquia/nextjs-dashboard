6dc0578bdfa6ea48270381c8e05898717bdb8ed0
Lorena Sucerquia <cristi.mayo@hotmail.com>
2024-08-16 10:43:19 -0500

    Conexion a versel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+
+ 
+
 export default function Page() {
     return <p>Dashboard Page</p>;
   }
+
+
